246eaca970dbfce50e0a810d9d6722340e67834e
SoraNoKage <llsoranokagell@gmail.com>
2024-01-04 18:23:27 +0300

    Set configuration

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
-# This is a sample Python script.
+import uvicorn
+from fastapi import FastAPI
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+from app.api.endpoints.todo import todo_router
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+app = FastAPI()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+app.include_router(todo_router)
+
+if __name__ == "__main__":
+    uvicorn.run(app="main:app", reload=True)
